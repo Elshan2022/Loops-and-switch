9521441c249d4675f30532ef1bbc0dcd94ff7736
Elshan2022 <h.elsen.h57@gmail.com>
2024-02-02 11:52:14 +0400

    Arrow function learning

--- task1.js
+++ task1.js
@@ -1,41 +1,67 @@
-function calculateSum(numbers) {
+let greet = (greeting = "Hello") => greeting;
+
+console.log(greet("Hello World"));
+
+//////////////////////////////////
+
+let factorial = (n) => {
+
+    if (n <= 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1)
+    }
+};
+
+console.log("Factorial of number", factorial(3));
+
+/////////////////////////////////////
+
+let sumAll = (...rest) => {
     let sum = 0;
-    for (i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
+    for (let i = 0; i < rest.length; i++) {
+
+        sum += rest[i];
+
     }
 
     return sum;
+};
+
+console.log("Sum of numbers", sumAll(2, 4, 6));
+
+
+////////////////////////////////////
+
+
+function f1() {
+    console.log("I'm F1 function");
+}
+function f2() {
+    console.log("I'm F2 function");
+
+}
+function f3() {
+    console.log("I'm F3 function");
+
+}
+function f4() {
+    console.log("I'm F4 function");
+    f5();
+
+}
+function f5() {
+    console.log("I'm F5 function");
 
 }
 
-let sum = calculateSum([2, 4, 6, 8, 10])
-
-console.log(sum);
-
-
-//////////////////////////
-
-let counter = 0;
-
-while (counter < 5) {
-    console.log(counter++);
+function callAllFunction() {
+    console.log("I'am common function");
+    f1();
+    f3();
+    f4();
+    
 }
 
+callAllFunction();
 
-/////////////////////////
-
-let cutDown = 5;
-
-do {
-    console.log(cutDown--);
-} while (cutDown > 0);
-
-/////////////////////
-
-function evaluateGrade(grade) {
-    let result = "";
-    result = grade > 60 ? "You passed exam" : "You failed exam";
-    console.log(result);
-}
-
-evaluateGrade(80);
