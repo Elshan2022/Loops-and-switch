4cbc38d23b5c55f8976c145b87d42aa8ccd77bd0
Elshan2022 <h.elsen.h57@gmail.com>
2024-02-14 13:07:51 +0400

    Higer order function

--- task1.js
+++ task1.js
@@ -1,66 +1,65 @@
-const stocks = [
-    {
-        price: 250,
-        sector: "IT",
-        symbol: "MGV",
-    },
-    {
-        price: 60,
-        sector: "HR",
-        symbol: "KKK",
-    },
-    {
-        price: 320,
-        sector: "HR",
-        symbol: "GTR",
-    },
-    {
-        price: 100,
-        sector: "BS",
-        symbol: "MDD",
-    },
-    {
-        price: 100,
-        sector: "BS",
-        symbol: "MDD",
-    },
+const customers = [
+    { id: 1, name: 'Alice', email: 'alice@example.com', location: { city: 'New York', country: 'USA' } },
+    { id: 2, name: 'Bob', email: 'bob@example.com', location: { city: 'Paris', country: 'France' } },
+    { id: 3, name: 'John', email: 'john@example.com', location: { city: 'Estonia', country: 'Tartu' } },
+    { id: 4, name: 'Fred', email: 'fred@example.com', location: { city: 'Germany', country: 'Berlin' } },
+    { id: 5, name: 'Luis', email: 'luis@example.com', location: { city: 'Russia', country: 'Moscow' } },
+
+
 ];
 
-function stockPriceUpdate() {
-
-    const interval = setInterval(() => {
-        const random = Math.random();
-        const index = Math.floor(random * stocks.length);
-        const percentageRandom = (random * 100).toFixed(1);
-        const selectedItem = stocks[index];
-        let selected = stocks.find((item) => {
-            return selectedItem === item;
-        });
-        let price = selected.price;
-        price = price + (percentageRandom * price) / 100;
-
-        console.log("Changing stock", selectedItem.price, selectedItem.symbol);
-
-    }, 2000);
+const orders = [
+    { orderId: 101, customerId: 1, product: 'Laptop', quantity: 1, price: 1200 },
+    { orderId: 102, customerId: 2, product: 'Smartphone', quantity: 2, price: 800 },
+    { orderId: 103, customerId: 3, product: 'Kettle', quantity: 3, price: 900 },
+    { orderId: 104, customerId: 4, product: 'Backpack', quantity: 4, price: 300 },
+    { orderId: 105, customerId: 5, product: 'Ironer', quantity: 5, price: 1600 },
+];
 
 
-
-    return setTimeout(() => {
-        clearInterval(interval);
-
-    }, 2002);
-
-}
-
-stockPriceUpdate();
-
-
-
-const specificStocks = stocks.filter((obj) => {
-    const requiredSector = "HR";
-    const matchedSector = obj.sector === requiredSector;
-    return matchedSector;
+const customersEmail = customers.map((object) => {
+    return object.email;
 });
 
-console.log("Specific stocks", specificStocks);
+console.log(customersEmail);
 
+const priceGreaterThousand = orders.filter((object) => {
+    const { price, quantity } = object;
+    return (price * quantity) > 1000;
+});
+
+console.log(priceGreaterThousand);
+
+console.log("-----------------------");
+
+const specificCustomer = customers.find((object) => {
+    return object.name === "Alice"
+});
+
+console.log(specificCustomer);
+
+console.log("----------------------");
+
+const specificOrderId = orders.findIndex((object) => {
+    return object.orderId === 102;
+});
+
+console.log(specificOrderId);
+
+console.log("----------------------");
+
+
+const isMatchCountry = customers.some((object) => {
+    return object.location.country === "USA";
+});
+
+console.log(isMatchCountry);
+
+console.log("----------------------");
+
+orders.forEach((orderObj) => {
+    let matchedObj = customers.find((cusObj) => orderObj.customerId === cusObj.id);
+
+    const { orderId, quantity, price, product } = orderObj;
+    console.log(`Order ${orderId} by ${matchedObj.name}: ${quantity} x ${product} for ${price} each`);
+});
